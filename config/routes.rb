0eaa0ff8a52548c54c0be26b1217b9f24dee1cce
snerva <selenenerva@live.it>
2023-03-08 14:52:56 +0100

    article crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+
+  
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # replace routes with only one
+
+  resources :articles
 end
