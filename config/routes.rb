097a29812327e5623138a668872575d1fc08cc7a
snerva <selenenerva@live.it>
2023-03-09 11:08:24 +0100

    auth devise login

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
-
-  
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-  # replace routes with only one
-
+  devise_for :users
   resources :articles do
    resources :comments
   end
